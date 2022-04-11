e8f27cc8b18f6251260add905d0fc753df0f5f6c
Valeria <patinalera@gmail.com>
2022-04-11 21:24:58 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "RTUweatherBot",
+    version = "1.0",
+    description = "Telegram bot about the weather",
+    url = "https://github.com/devilishgrapefruit/RTUweatherBot",
+    packages = find_packages()
+)
